687e2bb32dea37698ed3769d23d5ec3a279e17c2
Th3javo <javier1234salas@gmail.com>
2026-08-02 19:30:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const liveStatus = document.getElementById("live-status");
+const liveText = document.getElementById("live-text");
 
+function mostrarEstado() {
+  liveText.textContent = "STREAMING";
+}
+
+mostrarEstado();
